18ef01ea6bb56a2df3e6ade751ebcfd4f4aaf4ef
Anders Ryd <ar322@cornell.edu>
2020-04-27 16:19:14 -0400

    First version of splitting AllStubsMemory.h into .cc and .h

--- L1Trigger/TrackFindingTracklet/interface/AllStubsMemory.h
+++ L1Trigger/TrackFindingTracklet/interface/AllStubsMemory.h
@@ -2,26 +2,17 @@
 #ifndef L1Trigger_TrackFindingTracklet_interface_AllStubsMemory_h
 #define L1Trigger_TrackFindingTracklet_interface_AllStubsMemory_h
 
-#include "L1TStub.h"
-#include "Stub.h"
-#include "MemoryBase.h"
 
-#include <cctype>
+#include "L1Trigger/TrackFindingTracklet/interface/L1TStub.h"
+#include "L1Trigger/TrackFindingTracklet/interface/Stub.h"
+#include "L1Trigger/TrackFindingTracklet/interface/MemoryBase.h"
 
-using namespace std;
+#include <vector>
+
 
 class AllStubsMemory : public MemoryBase {
 public:
-  AllStubsMemory(string name, const Settings* const settings, unsigned int iSector, double phimin, double phimax) :
-  MemoryBase(name, settings, iSector) {
-    phimin_ = phimin;
-    phimax_ = phimax;
-
-    //set the layer or disk that the memory is in
-    initLayerDisk(3, layer_, disk_);
-
-    assert(name.substr(5, 3) == "PHI");
-  }
+  AllStubsMemory(string name, const Settings* const settings, unsigned int iSector, double phimin, double phimax);
 
   void addStub(std::pair<Stub*, L1TStub*> stub) { stubs_.push_back(stub); }
 
@@ -33,19 +24,7 @@ public:
 
   void clean() { stubs_.clear(); }
 
-  void writeStubs(bool first) {
-    openFile(first, "../data/MemPrints/Stubs/AllStubs_");
-
-    for (unsigned int j = 0; j < stubs_.size(); j++) {
-      string stub = stubs_[j].first->str();
-      out_ << "0x";
-      if (j < 16)
-        out_ << "0";
-      out_ << hex << j << dec;
-      out_ << " " << stub << " " << hexFormat(stub) << endl;
-    }
-    out_.close();
-  }
+  void writeStubs(bool first);
 
   int layer() const { return layer_; }
   int disk() const { return disk_; }
---
+++ L1Trigger/TrackFindingTracklet/src/AllStubsMemory.cc
@@ -0,0 +1,31 @@
+#include "L1Trigger/TrackFindingTracklet/interface/AllStubsMemory.h"
+
+using namespace std;
+using namespace Trklet;
+
+
+AllStubsMemory::AllStubsMemory(string name, const Settings* const settings, unsigned int iSector, double phimin, double phimax) :
+  MemoryBase(name, settings, iSector) {
+    phimin_ = phimin;
+    phimax_ = phimax;
+
+    //set the layer or disk that the memory is in
+    initLayerDisk(3, layer_, disk_);
+
+    assert(name.substr(5, 3) == "PHI");
+  }
+
+void AllStubsMemory::writeStubs(bool first) {
+    openFile(first, "../data/MemPrints/Stubs/AllStubs_");
+
+    for (unsigned int j = 0; j < stubs_.size(); j++) {
+      string stub = stubs_[j].first->str();
+      out_ << "0x";
+      if (j < 16)
+        out_ << "0";
+      out_ << hex << j << dec;
+      out_ << " " << stub << " " << hexFormat(stub) << endl;
+    }
+    out_.close();
+  }
+
