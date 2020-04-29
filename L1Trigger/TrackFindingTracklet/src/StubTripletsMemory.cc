adae7ad3b0a86f93b9b27538f3310ff85f9aae24
Louise Skinnari <louise.skinnari@cern.ch>
2020-04-29 15:20:00 +0200

    split StubTripletsMemory

---
+++ L1Trigger/TrackFindingTracklet/src/StubTripletsMemory.cc
@@ -0,0 +1,48 @@
+#include "L1Trigger/TrackFindingTracklet/interface/StubTripletsMemory.h"
+#include "L1Trigger/TrackFindingTracklet/interface/L1TStub.h"
+#include "L1Trigger/TrackFindingTracklet/interface/Stub.h"
+
+using namespace std;
+using namespace Trklet;
+
+StubTripletsMemory::StubTripletsMemory(string name, const Settings* const settings, unsigned int iSector, double phimin, double phimax) :
+  MemoryBase(name, settings, iSector) {
+  phimin_ = phimin;
+  phimax_ = phimax;
+}
+
+void StubTripletsMemory::writeST(bool first) {
+  std::string fname = "../data/MemPrints/StubPairs/StubTriplets_";
+  fname += getName();
+  fname += "_";
+  ostringstream oss;
+  oss << iSector_ + 1;
+  if (iSector_ + 1 < 10)
+    fname += "0";
+  fname += oss.str();
+  fname += ".dat";
+  if (first) {
+    bx_ = 0;
+    event_ = 1;
+    out_.open(fname.c_str());
+  } else
+    out_.open(fname.c_str(), std::ofstream::app);
+  
+  out_ << "BX = " << (bitset<3>)bx_ << " Event : " << event_ << endl;
+  
+  for (unsigned int j = 0; j < stubs1_.size(); j++) {
+    string stub1index = stubs1_[j].first->stubindex().str();
+    string stub2index = stubs2_[j].first->stubindex().str();
+    string stub3index = stubs3_[j].first->stubindex().str();
+    if (j < 16)
+      out_ << "0";
+    out_ << hex << j << dec;
+    out_ << " " << stub1index << "|" << stub2index << "|" << stub3index << endl;
+  }
+  out_.close();
+  
+  bx_++;
+  event_++;
+  if (bx_ > 7)
+    bx_ = 0;
+}
