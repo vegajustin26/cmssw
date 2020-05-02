72d3baa3a77e874429f21b87a507cbcb4f06dd13
Louise Skinnari <louise.skinnari@cern.ch>
2020-05-02 12:17:44 +0200

    split FPGAWord

---
+++ L1Trigger/TrackFindingTracklet/src/FPGAWord.cc
@@ -0,0 +1,129 @@
+#include "L1Trigger/TrackFindingTracklet/interface/FPGAWord.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+using namespace std;
+using namespace Trklet;
+
+FPGAWord::FPGAWord() {
+  value_ = -1;
+  nbits_ = -1;
+}
+
+FPGAWord::FPGAWord(int value, int nbits, bool positive, int line, const char* file) {
+  set(value, nbits, positive, line, file);
+}
+
+void FPGAWord::set(int value, int nbits, bool positive, int line, const char* file) {
+  value_ = value;
+  nbits_ = nbits;
+  positive_ = positive;
+  if (positive) {
+    if (value < 0) {
+      edm::LogProblem("Tracklet") << "FPGAWord got negative value:" << value << " (" << file << ":" << line << ")";
+    }
+    assert(value >= 0);
+  }
+  if (nbits >= 22) {
+    edm::LogPrint("Tracklet") << "FPGAWord got too many bits:" << nbits << " (" << file << ":" << line << ")";
+  }
+  assert(nbits < 22);
+  if (nbits <= 0) {
+    edm::LogPrint("Tracklet") << "FPGAWord got too few bits:" << nbits << " (" << file << ":" << line << ")";
+  }
+  assert(nbits > 0);
+  if (positive) {
+    if (value >= (1 << nbits)) {
+      if (file != 0) {
+	edm::LogProblem("Tracklet") << "value too large:" << value << " " << (1 << nbits) << " (" << file << ":" << line << ")";
+      }
+    }
+    assert(value < (1 << nbits));
+  } else {
+    if (value > (1 << (nbits - 1))) {
+      edm::LogProblem("Tracklet") << "value too large:" << value << " " << (1 << (nbits - 1)) << " (" << file << ":" << line << ")";
+    }
+    assert(value <= (1 << (nbits - 1)));
+    if (value < -(1 << (nbits - 1))) {
+      edm::LogProblem("Tracklet") << "value too negative:" << value << " " << -(1 << (nbits - 1)) << " (" << file << ":" << line << ")";
+    }
+    assert(value >= -(1 << (nbits - 1)));
+  }
+}
+
+std::string FPGAWord::str() const {
+    const int nbit = nbits_;
+
+    if (!(nbit > 0 && nbit < 22))
+      edm::LogVerbatim("Tracklet") << "nbit: " << nbit;
+    if (nbit == -1)
+      return "?";
+    if (nbit == 0)
+      return "~";
+
+    assert(nbit > 0 && nbit < 22);
+
+    std::ostringstream oss;
+    if (nbit == 1)
+      oss << (bitset<1>)value_;
+    if (nbit == 2)
+      oss << (bitset<2>)value_;
+    if (nbit == 3)
+      oss << (bitset<3>)value_;
+    if (nbit == 4)
+      oss << (bitset<4>)value_;
+    if (nbit == 5)
+      oss << (bitset<5>)value_;
+    if (nbit == 6)
+      oss << (bitset<6>)value_;
+    if (nbit == 7)
+      oss << (bitset<7>)value_;
+    if (nbit == 8)
+      oss << (bitset<8>)value_;
+    if (nbit == 9)
+      oss << (bitset<9>)value_;
+    if (nbit == 10)
+      oss << (bitset<10>)value_;
+    if (nbit == 11)
+      oss << (bitset<11>)value_;
+    if (nbit == 12)
+      oss << (bitset<12>)value_;
+    if (nbit == 13)
+      oss << (bitset<13>)value_;
+    if (nbit == 14)
+      oss << (bitset<14>)value_;
+    if (nbit == 15)
+      oss << (bitset<15>)value_;
+    if (nbit == 16)
+      oss << (bitset<16>)value_;
+    if (nbit == 17)
+      oss << (bitset<17>)value_;
+    if (nbit == 18)
+      oss << (bitset<18>)value_;
+    if (nbit == 19)
+      oss << (bitset<19>)value_;
+    if (nbit == 20)
+      oss << (bitset<20>)value_;
+    if (nbit == 21)
+      oss << (bitset<21>)value_;
+    if (nbit == 22)
+      oss << (bitset<22>)value_;
+
+    return oss.str();
+}
+
+unsigned int FPGAWord::bits(unsigned int lsb, unsigned int nbit) {
+  assert(lsb + nbit <= (unsigned int)nbits());
+  return (value_ >> lsb) & ((1 << nbit) - 1);
+}
+
+bool FPGAWord::atExtreme() const {
+  if (positive_) {
+    return (value_ == 0) || (value_ == (1 << nbits_) - 1);
+  }
+  return ((value_ == (-(1 << (nbits_ - 1)))) || (value_ == ((1 << (nbits_ - 1)) - 1)));
+}
+
+bool FPGAWord::operator==(const FPGAWord& other) const {
+  return (value_ == other.value_) && (nbits_ == other.nbits_) && (positive_ == other.positive_);
+}
