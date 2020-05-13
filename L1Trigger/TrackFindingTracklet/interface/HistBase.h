a06f8d2eb36472c5f88d2c43b7466479b10638ea
Louise Skinnari <louise.skinnari@cern.ch>
2020-05-13 02:06:28 +0200

    clean up

--- L1Trigger/TrackFindingTracklet/interface/HistBase.h
+++ L1Trigger/TrackFindingTracklet/interface/HistBase.h
@@ -21,13 +21,9 @@ namespace trklet {
 
     virtual void close() {}
 
-    virtual void FillLayerResidual(int, int, double, double, double, double, bool) {
-      return;  //default implementation does nothing
-    }
+    virtual void FillLayerResidual(int, int, double, double, double, double, bool) {}
 
-    virtual void FillDiskResidual(int, int, double, double, double, double, bool) {
-      return;  //default implementation does nothing
-    }
+    virtual void FillDiskResidual(int, int, double, double, double, double, bool) {}
 
     //arguments are
     // int seedIndex
@@ -38,16 +34,12 @@ namespace trklet {
     // double iz0, z0
     // int tp
     virtual void fillTrackletParams(
-        const Settings*, Globals*, int, int, double, double, double, double, double, double, double, double, int) {
-      return;  //default implementation does nothing
-    }
+        const Settings*, Globals*, int, int, double, double, double, double, double, double, double, double, int) {}
 
     //int seedIndex
     //double etaTP
     //bool eff
-    virtual void fillSeedEff(int, double, bool) {
-      return;  //default implementation does nothing
-    }
+    virtual void fillSeedEff(int, double, bool) {}
 
   private:
   };
